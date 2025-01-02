f7a0e182768c1c36209b801703027e8dbdd393c2
Aaby Abhay <aaby242abhay@gmail.com>
2025-01-03 00:08:30 +0530

    implementing the string class from scratch

--- Cpp/14.cpp
+++ Cpp/14.cpp
@@ -7,22 +7,43 @@ class String{
         int size;
 
     public: 
-        String(const char* str = NULL);
-        ~String(){ delete[] s;}
-        String(const String&);
+        String(const char* str = NULL);     //this is the constructor
+        ~String(){ delete[] s;}             //this is the destuctor
+        String(const String&);              //this is the copy constructor
         void print(){
             cout << s << endl;
         }
-        void change(const char*);
+        void change(const char*);           //This is a function declaration
 };
 
 String::String(const char* str){
+    
     size = strlen(str);
     s = new char[size+1];
     strcpy(s, str);
 }
 
+void String::change(const char* str){
+    delete[] s;
+    size = strlen(str);
+    s = new char[size+1];
+    strcpy(s, str);
+}
+
+String :: String(const String& old_str){
+    size = old_str.size;
+    s = new char[size+1];
+    strcpy(s, old_str.s);
+}
 
 int main(){
+    String str1("Aaaby Abhay");
+    String str2 = str1;
+    str1.print();
+    str2.print();
 
+    str2.change("Aditi Singh");
+    str1.print();
+    str2.print();
+    return 0;
 }
